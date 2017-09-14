407e979bf895666556eb071d251545d3d646ebd7
Eric Li <Eric.Li@nafinc.com>
2017-09-14 13:12:56 -0700

    added auth

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,10 @@
 
 const noteRoutes = require('./note_routes');
 const productsRoutes=require('./products_routes');
+const loginRoutes=require('./login_routes');
 module.exports = function(app, db) {
   noteRoutes(app, db);
   // Other route groups could go here, in the future
   productsRoutes(app,db);
+  loginRoutes(app,db);
 };
